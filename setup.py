b4c5a8318ef058d078cafd0017a5f59575a700cb
rafaelgss <rafael.nunu@hotmail.com>
2018-09-03 21:43:15 -0300

    up pymysql setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pymysql.wrapper',
+    version='0.0.5',
+    packages=[''],
+    url='',
+    license='',
+    author='RafaelGSS',
+    author_email='rafael.nunu@hotmail.com',
+    description=''
+)
